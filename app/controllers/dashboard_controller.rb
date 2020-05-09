e903bdca6534bb2882995a49c838a9fde608d26f
Leo Arruda <lvarruda@gmail.com>
2020-05-09 15:45:37 -0400

    Profile edit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,10 @@ class DashboardController < ApplicationController
   def index
   end
 
+  def profile
+    @account = Account.find(params[:id])
+  end
+
   def set_sidebar
     @show_sidebar = true
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,13 +18,4 @@ module ApplicationHelper
         end
         "<script>$(function(){ #{ flash_messages.join("\n") } });</script>".html_safe if flash_messages.any?
     end
-
-    def toastr_flash
-        flash.each_with_object([]) do |(type, message), flash_messages|
-          type = 'success' if type == 'notice'
-          type = 'error' if type == 'alert' || type=="danger"
-          text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true })</script>"
-          flash_messages << text.html_safe if message
-        end.join("\n").html_safe
-    end
 end
