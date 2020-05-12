b217b61afd9b631598f783e9a6b629aada4c3e81
Leo Arruda <lvarruda@gmail.com>
2020-05-11 20:55:00 -0400

    Profile

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,4 +4,8 @@ class Property < ApplicationRecord
 
     scope :latest, -> { order created_at: :desc}
     # Ex:- scope :active, -> {where(:active => true)}
+    scope :sold, -> { where(for_sale: true, status: "sold") }
+    scope :for_sale, -> { where(for_sale: true, status: "available") }
+    scope :leased, -> { where(for_sale: false, status: "leased") }
+    scope :for_rent, -> { where(for_sale: false, status: "available") }
 end
